3e76c9e845a9380f48e5c9e5128a454d1bb43d1e
IanChen <ian806.chen@gmail.com>
2022-05-30 09:35:59 +0800

    commit.

---
+++ PollyRateLimit/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Polly;
+
+namespace PollyRateLimit.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    private static readonly Policy<IEnumerable<WeatherForecast>>? WithPolicyKey =
+        Policy.RateLimit<IEnumerable<WeatherForecast>>(1, TimeSpan.FromMinutes(1))
+            .WithPolicyKey("key");
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return WithPolicyKey!.Execute(() => WeatherForecasts());
+    }
+
+    private static WeatherForecast[] WeatherForecasts()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+}
